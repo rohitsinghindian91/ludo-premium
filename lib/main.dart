399b068e54bfdcf63a349f8b87409125f3afc5dc
rohitsinghindian91 <rohitsinghindian91@gmail.com>
2026-09-18 06:30:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,4 @@
 import 'package:flutter/material.dart';
-void main(){runApp(MaterialApp(home: Scaffold(body: Center(child: Text("Ludo Premium - Kumar131@fam")))));}
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text("Test OK")))));
+}
