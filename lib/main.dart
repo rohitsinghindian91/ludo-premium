c3895dbf52c6f857305441813c6b93e8e4752d2d
rohitsinghindian91 <rohitsinghindian91@gmail.com>
2026-09-18 06:44:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,25 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("Ludo Premium OK")))));
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: PayScreen()));
+
+class PayScreen extends StatelessWidget {
+  final upi = "Kumar131@fam";
+  void pay() async {
+    final uri = Uri.parse("upi://pay?pa=$upi&pn=Ludo Premium&am=500&cu=INR&tn=Premium 30 Days");
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Ludo Premium"), backgroundColor: Colors.green),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: pay,
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.green, minimumSize: Size(250,60)),
+          child: Text("500rs Pay - $upi", style: TextStyle(fontSize: 20, color: Colors.white)),
+        ),
+      ),
+    );
+  }
 }
