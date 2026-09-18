c8120414faf443f5b6f3b96e85b9e0d881cc1afc
rohitsinghindian91 <rohitsinghindian91@gmail.com>
2026-09-18 06:36:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("Ludo Premium OK")))));
+}
