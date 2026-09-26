b9ea0d46ac1b962a84ddf0b67aef6149835be12d
rohitsinghindian91 <rohitsinghindian91@gmail.com>
2026-09-26 18:55:27 +0530

    Update rtdb.dart

--- lib/rtdb.dart
+++ lib/rtdb.dart
@@ -2,14 +2,12 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 FirebaseDatabase? _rtdb;
-
 FirebaseDatabase getRtdb() {
   if (_rtdb == null) {
     _rtdb = FirebaseDatabase.instanceFor(
       app: Firebase.app(),
       databaseURL: "https://ludo-premium-50-e427e-default-rtdb.asia-southeast1.firebasedatabase.app",
     );
-    // Persistence hata diya hai - isi se T0/T1 desync ho raha tha
     _rtdb!.goOnline();
   }
   return _rtdb!;
